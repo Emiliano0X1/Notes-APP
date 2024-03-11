eaadad32e6de5e44b4642eafbb89c37f289d88a6
Emiliano0X1 <146475390+Emiliano0X1@users.noreply.github.com>
2024-03-11 08:17:30 -0700

    Add files via upload

---
+++ NotasApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotasApp.swift
+//  Notas
+//
+//  Created by CETYS Universidad  on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NotasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
